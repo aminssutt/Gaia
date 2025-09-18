93a9206cdde654a76b57c5962bb0d159d5baa079
BERACHE Lakhdar/베라체락다르 <lakhdar.berache@renault.com>
2025-09-18 16:29:01 +0900

    fix problem of deploy

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? '/Gaia/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
